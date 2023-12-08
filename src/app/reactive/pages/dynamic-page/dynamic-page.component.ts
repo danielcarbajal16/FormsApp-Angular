aa352959bf711b0f3058c0c37e77928e3ec22892
Eduardo Daniel Carbajal <eduardo.carbajal@jalisco.gob.mx>
2023-12-08 17:05:39 -0600

    Dynamic form validations

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   ]
 })
 export class DynamicPageComponent {
+  public myForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ])
+  })
+  
+  constructor(private fb: FormBuilder) {}
 
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  isInvalidField(field: string): boolean | null | undefined {
+    return this.myForm.get(field)?.errors && this.myForm.get(field)?.touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.get(field)) {
+      return null;
+    }
+
+    const errors = this.myForm.get(field)?.errors || {};
+    for (const key of Object.keys(errors)) {
+      switch(key) {
+        case 'required': return 'Este campo es requerido';
+        case 'minlength': return `Este campo requiere un mínimo de ${errors['minlength'].requiredLength} caracteres`;
+      }
+    }
+
+    return null;
+  }
+
+  isInvalidFieldInArray(formArray: FormArray, index: number) {
+    return formArray.controls[index].errors && formArray.controls[index].touched;
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 }
