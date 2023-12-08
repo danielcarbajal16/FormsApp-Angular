832621762cad352326dde90f3fbb18e3e5119a03
Eduardo Daniel Carbajal <eduardo.carbajal@jalisco.gob.mx>
2023-12-08 15:56:32 -0600

    Validations to show error messages

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   ]
 })
 export class BasicPageComponent {
+  public myForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    price: new FormControl(0, [Validators.required, Validators.min(0)]),
+    inStorage: new FormControl(0, [Validators.required, Validators.min(0)])
+  });
 
+  onSave(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    
+    console.log(this.myForm.value);
+    this.myForm.reset({ price: 0, inStorage: 0 });
+  }
+
+  isInvalidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) {
+      return null;
+    }
+
+    const errors = this.myForm.controls[field].errors || {};
+    for (const key of Object.keys(errors)) {
+      switch(key) {
+        case 'required': return 'Este campo es requerido';
+        case 'minlength': return `Este campo requiere un mínimo de ${errors['minlength'].requiredLength} caracteres`;
+      }
+    }
+
+    return null;
+  }
 }
--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { ReactiveRoutingModule } from './reactive-routing.module';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
 
 
@@ -15,6 +16,7 @@ import { SwitchesPageComponent } from './pages/switches-page/switches-page.compo
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ReactiveRoutingModule
   ]
 })
