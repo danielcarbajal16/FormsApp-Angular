4f2117d11aeb5e6a7d2382dc6f0b778dc401783e
Eduardo Daniel Carbajal <eduardo.carbajal@jalisco.gob.mx>
2023-12-11 11:55:04 -0600

    New forms manage functions

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -13,7 +13,9 @@ export class DynamicPageComponent {
       ['Metal Gear', Validators.required],
       ['Death Stranding', Validators.required],
     ])
-  })
+  });
+
+  public newFavorite: FormControl = this.fb.control('', Validators.required);
   
   constructor(private fb: FormBuilder) {}
 
@@ -45,6 +47,17 @@ export class DynamicPageComponent {
     return formArray.controls[index].errors && formArray.controls[index].touched;
   }
 
+  onDeleteFavorite(index: number): void {
+    this.favoriteGames.removeAt(index);
+  }
+
+  onAddFavorite(): void {
+    if (this.newFavorite.invalid) return;
+
+    this.favoriteGames.push(this.fb.control(this.newFavorite.value, Validators.required));
+    this.newFavorite.reset();
+  }
+
   onSubmit(): void {
     if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
@@ -52,6 +65,7 @@ export class DynamicPageComponent {
     }
 
     console.log(this.myForm.value);
+    (this.myForm.controls['favoriteGames'] as FormArray) = this.fb.array([]);
     this.myForm.reset();
   }
 }
