580af8df24535a3bd2f33e4e0034d67200953370
Eduardo Daniel Carbajal <eduardo.carbajal@jalisco.gob.mx>
2023-12-12 10:19:47 -0600

    Sync validators created, 2 options, for functions or service

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+//import { cantBeStrider, emailPattern, firstNameAndLastnamePattern } from 'src/app/shared/validators/validators';
+import { ValidatorsService } from 'src/app/shared/services/validators.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern(ValidatorsService.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(ValidatorsService.emailPattern)]],
+    username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
 
+  constructor(private fb: FormBuilder, private validatorsService: ValidatorsService) {}
+
+  isValidField(field: string) {
+    return this.validatorsService.isValidField(this.myForm, field);
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: [
   ]
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [false, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue]
+  });
 
+  public person = {
+    gender: 'F',
+    wantNotifications: false
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+      this.myForm.reset({
+        ...this.person,
+        termsAndConditions: false
+      })
+  }
+
+  onSave(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+    this.person = newPerson;
+    console.log(this.myForm.value);
+    console.log(this.person);
+  }
+
+  isInvalidField(field: string): boolean | null | undefined {
+    return this.myForm.get(field)?.errors && this.myForm.get(field)?.touched;
+  }
 }
