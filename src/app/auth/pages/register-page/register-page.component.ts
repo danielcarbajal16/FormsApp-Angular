d88c2661f500a08eb3700bb15d6ba13738e3ff8b
Eduardo Daniel Carbajal <eduardo.carbajal@jalisco.gob.mx>
2023-12-12 13:09:50 -0600

    New async validator and function created to show error message depending error type

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 //import { cantBeStrider, emailPattern, firstNameAndLastnamePattern } from 'src/app/shared/validators/validators';
 import { ValidatorsService } from 'src/app/shared/services/validators.service';
+import { EmailValidator } from 'src/app/shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -12,13 +13,15 @@ import { ValidatorsService } from 'src/app/shared/services/validators.service';
 export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.pattern(ValidatorsService.firstNameAndLastnamePattern)]],
-    email: ['', [Validators.required, Validators.pattern(ValidatorsService.emailPattern)]],
+    email: ['', [Validators.required, Validators.pattern(ValidatorsService.emailPattern)], [this.emailValidator]],
     username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
+  }, {
+    validators: [ this.validatorsService.isFieldOneEqualFieldTwo('password', 'password2') ]
   });
 
-  constructor(private fb: FormBuilder, private validatorsService: ValidatorsService) {}
+  constructor(private fb: FormBuilder, private validatorsService: ValidatorsService, private emailValidator: EmailValidator) {}
 
   isValidField(field: string) {
     return this.validatorsService.isValidField(this.myForm, field);
@@ -27,4 +30,26 @@ export class RegisterPageComponent {
   onSubmit() {
     this.myForm.markAllAsTouched();
   }
+
+  errorMessage(field: string): string | null {
+    if (!this.myForm.get(field)) {
+      return null;
+    }
+    
+    const errors = this.myForm.get(field)?.errors || {};
+    
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required': return 'Este campo es requerido'
+        case 'pattern': return 'Formato de email no válido';
+        case 'emailTaken': return 'Email ya tomado';
+        case 'noStrider': return 'El username no puede ser Strider';
+        case 'minlength': return `Este campo requiere un mínimo de ${errors['minlength'].requiredLength} caracteres`;
+        case 'notEqual': return 'Las contraseñas deben de ser iguales';
+      }
+    }
+    
+    return null;
+  }
+
 }
--- src/app/shared/services/validators.service.ts
+++ src/app/shared/services/validators.service.ts
@@ -21,4 +21,19 @@ export class ValidatorsService {
     isValidField(form: FormGroup, field: string) {
         return form.controls[field].errors && form.controls[field].touched;
     }
+
+    isFieldOneEqualFieldTwo(field1: string, field2: string) {
+        return (formGroup: FormGroup): ValidationErrors | null => {
+            const fieldValue1 = formGroup.get(field1)?.value;
+            const fieldValue2 = formGroup.get(field2)?.value;
+
+            if (fieldValue1 !== fieldValue2) {
+                const errors = formGroup.get(field2)?.errors || {};
+                formGroup.get(field2)?.setErrors({ ...errors, notEqual: true });
+                return { notEqual: true }
+            }
+
+            return null;
+        }
+    }
 }
